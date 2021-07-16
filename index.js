07225abd642b11054463dbd6ce7e28577a00b5a3
Nashi-developer <87036503+Nashi-developer@users.noreply.github.com>
2021-07-16 18:26:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => console.log("BOT IS READY"))
 
-client.login("YOUR TOKEN GOES HERE")
+client.login("ODYzNzk1MzkxMjY2OTQ3MTIy.YOsGJQ.agxQ01q3HuMOh9Dbfcdxc0qsnsg")
 
 require("./roster")(client)
 
